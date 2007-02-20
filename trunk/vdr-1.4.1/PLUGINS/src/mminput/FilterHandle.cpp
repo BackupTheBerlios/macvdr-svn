461e276313855c861ef72ef25ff04c6ac4fd02d2
wache <wache@d4865de3-4216-0410-b1cc-ea709ce1c1b5>
2007-02-20 10:00:58 +0000

    - cleanup and more checks

--- trunk/vdr-1.4.1/PLUGINS/src/mminput/FilterHandle.cpp
+++ trunk/vdr-1.4.1/PLUGINS/src/mminput/FilterHandle.cpp
@@ -1,10 +1,7 @@
 #include "FilterHandle.hpp"
 #include <unistd.h>
 #include <string.h>
-
-//#include <vdr/tools.h>
-//#include "DPConnect/TSHandler.hpp"
-//#include "DPConnect/Streaming.hpp"
+#include <inttypes.h>
 
 //#define FILDEB(out...) printf(out)
 
@@ -57,110 +54,101 @@ static inline void Dump(uint8_t *data) {
 };
 
 bool cSectionFilter::Process(int fd, uint8_t *tspkt) {
-//		TSHeader & tsh = *(TSHeader*)tspkt;
-
-		if( PUSI(tspkt) == 0 && WaitForNewSection == true){
-//			printf("cSectionFilter::Process: wait for new section\n");
-			return true;
-		}
-		
         int len=PayloadLength(tspkt);
-		int cc = tspkt[3] & 0x0f;
-
+        int cc = tspkt[3] & 0x0f;
+        bool cc_ok=((last_cc + 1) & 0x0f) == cc;
+        last_cc=cc;
+        
         if (!len)
                 return true;
 
-        int p_start=188-len;
+        if( PUSI(tspkt) == 0 && WaitForNewSection == true){
+                //printf("cSectionFilter::Process: wait for new section\n");
+                return true;
+        }
 
-        bool cc_ok=((last_cc + 1) & 0x0f) == cc;
-//		printf("got packet cc: %d cc_ok: %d  len %d \n",cc, cc_ok, len);
-        //Dump(tspkt);
-        last_cc=cc;
 
         if ( ((tspkt[3] & 0x20) && (tspkt[4]>0) && (tspkt[5] & 0x80)) 
              // adaption field and discontinuity indicator present
              || !cc_ok) { // or discontinuity detected
-                pos=0;
-				if(WaitForNewSection == false){
-                FILDEB("discontinuity..\n");
-				}
-				WaitForNewSection = true;
-				pos = 0;
-				return true;
+                if(WaitForNewSection == false){
+                        FILDEB("discontinuity..\n");
+                }
+                WaitForNewSection = true;
+                pos = 0;
+                return true;
         }
 
+        int p_start=188-len;
         if (PUSI(tspkt) ) {
-			FILDEB("cSectionFilter::Process: start new section..\n");		
-			WaitForNewSection = false;
+                FILDEB("cSectionFilter::Process: start new section..\n");		
+                WaitForNewSection = false;
                 uint8_t *remainder_start=&tspkt[p_start+1];
                 int remainder_len=tspkt[p_start];
+                if (p_start+1+remainder_len>188) {
+                        printf("cSectionFilter::Process:remainder_len too large (%d)!! \n",remainder_len);
+                        WaitForNewSection = true;
+                        pos = 0;
+                        return true;
+                }
 
-                uint8_t *new_start=&remainder_start[remainder_len];
-                int new_len= len-1-remainder_len;
-//				sectionNumRead = tspkt[p_start+2];
-//				printf("found start of packet remainder_len %d new_len %d\n", 
-//							remainder_len, new_len);
-                if (!pos) {
-                        //no packet to finish
-						if(pos + new_len >= MaxEPGSectionLength){
-							printf("estimated section too long (pos == %d)!! %d\n",pos, pos + new_len);
-							return false;
-						}
-//						printf("cSectionFilter::Process:section number: %d \n",sectionNumRead);
-                        memcpy(&buffer[pos],new_start,new_len);
-                        pos+=new_len;
-                } else {
-//						printf("cSectionFilter::Process:section number: %d \n",sectionNumRead);
-						memcpy(&buffer[pos],remainder_start,remainder_len);
+                if (pos) {
+                        // finish the old sectioin
+                        if( (pos + remainder_len >= MaxEPGSectionLength)){
+                                // section is longer than MaxSectionLength, check for padding bytes
+                                // and leave them out...
+                                int tmpLen = MaxEPGSectionLength - pos - 1;
+                                if(tspkt[p_start + tmpLen] != 0xFF){
+                                        printf("cSectionFilter::Process:estimated section too long (pos = %d)!! %d\n",pos, pos + len);
+                                        WaitForNewSection = true;
+                                        pos = 0;
+                                        return true;
+                                }
+                                len = tmpLen;
+                        }
+                        memcpy(&buffer[pos],remainder_start,remainder_len);
                         pos+=remainder_len;
 
                         int section_len = (((buffer[1] & 0x0F) << 8) | (buffer[2] & 0xFF)) + 3;
-                        //Dump(buffer);
-                        if (pos >= section_len) {
-							
+                        if (pos == section_len || 
+                            (pos > section_len && buffer[section_len] == 0xFF ) ) {
+                                // write the complete section to the pipe
                                 if (section_len != write(fd,buffer,section_len) ) {
                                         // FIXME check errno
                                         return false;
                                 }
-								
-                                //fsync(fd);
-                                //usleep(5000);
                         } else {
-//                                printf("pos %d section_len %d\n",pos,section_len);
-//                                Dump(buffer);
-//                                printf("cSectionFilter::Process:was mach ich hier?\n");
+                                // the previos section should be finished, but the size is wrong..
+                                // Discard the section.
+                                printf("cSectionFilter::Process:was mach ich hier? section_len %d, pos %d\n", section_len, pos);
+                                Dump(&buffer[section_len -5]);
 
-                                int section_start=section_len;
-                                section_len = (((buffer[section_start+1] & 0x0F) << 8) | (buffer[section_start+2] & 0xFF)) + 3;
-//                                printf("2:pos %d section_len %d\n",pos,section_len);
-                                Dump(&buffer[section_start-5]);
-                                pos=0;
-                        }                        
-                        pos=0;
-                        // start new section
-						if(pos + new_len >= MaxEPGSectionLength){
-							printf("cSectionFilter::Process:estimated section too long (pos =%d)!! %d\n",pos, pos + new_len);
-							return true;
-						}
-                        memcpy(&buffer[pos],new_start,new_len);
-                        pos+=new_len;
+                        }        
+                };
+                
+                // start new section
+                pos=0;
+                int new_len= len-1-remainder_len;
+                memcpy(&buffer[pos],&remainder_start[remainder_len],new_len);
+                pos+=new_len;
+                return true;
+        } 
+        
+        //printf("copy payload...\n");
+        if( (pos + len >= MaxEPGSectionLength)){
+                // section is longer than MaxSectionLength, check for padding bytes
+                // and leave them out...
+                int tmpLen = MaxEPGSectionLength - pos - 1;
+                if(tspkt[p_start + tmpLen] != 0xFF){
+                        printf("cSectionFilter::Process:estimated section too long (pos = %d)!! %d\n",pos, pos + len);
+                        WaitForNewSection = true;
+                        pos = 0;
+                        return true;
                 }
-        } else {
-			if(WaitForNewSection == false){ 
-                //printf("copy payload...\n");
-				if( (pos + len >= MaxEPGSectionLength)){
-
-					int tmpLen = MaxEPGSectionLength - pos - 1;
-					if(tspkt[p_start + tmpLen] != 0xFF){
-						printf("cSectionFilter::Process:estimated section too long (pos = %d)!! %d\n",pos, pos + len);
-						return true;
-					}
-					len = tmpLen;
-				}
-                memcpy(&buffer[pos],&tspkt[p_start],len);
-                pos+=len;
-			}
-        };
+                len = tmpLen;
+        }
+        memcpy(&buffer[pos],&tspkt[p_start],len);
+        pos+=len;
         return true;
 };
 
@@ -181,19 +169,13 @@ cFilterHandle::~cFilterHandle(){
 	}
 }
 
-/*
-	assum that eaach filter will create o
-*/
+// open a pipe for each filter and return it to vdr,
+// but only the first filter which matches a pid will recieve sections.
+// This should not be a problem, since vdr distributes the sections for us :-)
 int cFilterHandle::CreateFilter(int Pid, int Tid){
         int pos=0;
         FILDEB("CreateFilter pid 0x%02x tid 0x%02x \n",Pid,Tid);
 
-/*        if (Pid != 0x12) {
-                printf("stop 0x%x\n",Pid);
-                return -1;
-        };
-*/
-        
         // check for closed pipes
         for (pos=0; pos < MAXDEVICEFILTER ; pos++)
                 if ( PidNum[pos] != -1) {
@@ -214,9 +196,6 @@ int cFilterHandle::CreateFilter(int Pid, int Tid){
 
         if (pos >= MAXDEVICEFILTER) {
                 fprintf(stderr," Too many open filters! \n");
-                for (int i=0; i<MAXDEVICEFILTER; i++)
-                        fprintf(stderr,"0x%02x ",PidNum[pos]);
-                fprintf(stderr,"\n");
                 return -1; 	
         };
 	FILDEB("cFilterHandle::CreateFilter: Create Pipe for Filter.\n");
@@ -239,20 +218,21 @@ int cFilterHandle::Process(uchar* data){
         int pid =  ((data[1] & 0x1f) << 8) + data[2];
         //FILDEB("Process %p pid 0x%02x\n",data,pid);	
 
-        for(int i = 0; i < MAXDEVICEFILTER; i++){
-                //printf("Pidnum[%d] 0x%02x\n",i,PidNum[pos]);
-                if (PidNum[i] == pid){ 	
-                        if ( !FH[i].sf->Process(FH[i].Whandle,data) ) {
-                                // close pipe
-                                FILDEB("Close Filter pid 0x%02x, tid 0x%02x \n",
-                                        PidNum[i],FH[i].Tid);
-                                PidNum[i]=-1;
-                                delete FH[i].sf;
-                                FH[i].sf=NULL;
-                                close(FH[i].Whandle);
-                        };
-                        return 1;
-                }
-        }
-        return 0;
+        int i=0;
+        while (i<MAXDEVICEFILTER && PidNum[i]!=pid)
+                i++;
+        
+        if (i>=MAXDEVICEFILTER)
+                return 0;
+        
+        if ( !FH[i].sf->Process(FH[i].Whandle,data) ) {
+                // write to pipe failed, close it
+                FILDEB("Close Filter pid 0x%02x, tid 0x%02x \n",
+                                PidNum[i],FH[i].Tid);
+                PidNum[i]=-1;
+                delete FH[i].sf;
+                FH[i].sf=NULL;
+                close(FH[i].Whandle);
+        };
+        return 1;
 }
